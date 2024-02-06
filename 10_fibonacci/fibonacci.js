85bceccb6fcb817339b43b7a171183936d579075
xgsarah <sjbquimpo@gmail.com>
2024-02-06 17:30:00 +0800

    implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+  const num = Number(x);
 
+  if (num < 0) {
+    return 'OOPS';
+  }
+  if (num === 0) {
+    return num;
+  }
+  const arr = [1];
+  for (let i = 0; i < num; i++) {
+    if (i === 0) {
+      arr.push(i + arr[i]);
+    } else {
+      arr.push(arr[i] + arr[i - 1]);
+    }
+  }
+  return arr[num - 1];
 };
 
 // Do not edit below this line
