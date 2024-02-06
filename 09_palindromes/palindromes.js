c90c57ccf19c88d5f22ebe762f1438aa30328738
xgsarah <sjbquimpo@gmail.com>
2024-02-06 16:35:41 +0800

    implement palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const arr = string
+    .split('')
+    .filter(
+      (char) =>
+        char !== '' &&
+        char !== ',' &&
+        char !== ' ' &&
+        char !== '.' &&
+        char !== '!'
+    )
+    .map((char) => char.toLowerCase());
+  console.log('res:', arr.join(''), arr.reverse().join(''));
 
+  return arr.join('') === arr.reverse().join('');
 };
 
 // Do not edit below this line
