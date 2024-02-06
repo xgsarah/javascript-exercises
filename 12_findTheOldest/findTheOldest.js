4b6c84eebfe4078582c0ad0b42cd7679dbf18de3
xgsarah <sjbquimpo@gmail.com>
2024-02-06 18:37:42 +0800

    implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson = people[0];
+  
+  people.map((person) => {
+    const currentDate = new Date();
 
+    const personAge =
+      (person?.yearOfDeath || currentDate.getFullYear()) - person.yearOfBirth;
+
+    const oldestPersonAge =
+      (oldestPerson?.yearOfDeath || currentDate.getFullYear()) -
+      oldestPerson.yearOfBirth;
+
+    oldestPerson = personAge > oldestPersonAge ? person : oldestPerson;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
